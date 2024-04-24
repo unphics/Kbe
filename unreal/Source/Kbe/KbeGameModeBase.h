82a5917f1a1efcfe5c35729f2de4541ad4623e8f
unphics <481381521@qq.com>
2024-04-25 00:18:47 +0800

    创建客户端工程

---
+++ unreal/Source/Kbe/KbeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "KbeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KBE_API AKbeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
